fa2aa693229d41784eac390870878bf7d1d70b81
Danila Kritsky <danilakritsky@gmail.com>
2022-05-20 10:49:19 +0000

    Complete 1.8: unicafe step3

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,39 @@
 import { useState } from 'react'
 
+const Button = ({ text, handler}) => {
+  return <button onClick={handler}>{text}</button>
+}
+
+const Counter = ({ text, count }) => <div>{text} {count}</div>
+
+const Statistics = ({ good, neutral, bad}) => {
+  return (
+    <>
+      <h1>statistics</h1>
+      <div>
+        <Counter text="good" count={good} />
+        <Counter text="neutral" count={neutral} />
+        <Counter text="bad" count={bad} />
+        <Counter text="all" count={good + neutral + bad} />
+        <Counter
+          text="average"
+          count={
+            (good + neutral + bad) === 0
+              ? "-"
+              : (good + neutral * 0 + bad * -1) / (good + neutral + bad)}
+        />
+        <Counter
+          text="postive"
+          count={
+            (good + neutral + bad) === 0
+              ? "-"
+              : (good / (good + neutral + bad) * 100) + " %"}
+        />
+      </div>
+    </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -14,35 +48,9 @@ const App = () => {
       <Button text="good" handler={() => setGood(good + 1)} />
       <Button text="neutral" handler={() => setNeutral(neutral + 1)} />
       <Button text="bad" handler={() => setBad(bad + 1)} />
-      <h1>
-        statistics
-      </h1>
-      <Counter text="good" count={good} />
-      <Counter text="neutral" count={neutral} />
-      <Counter text="bad" count={bad} />
-      <Counter text="all" count={good + neutral + bad} />
-      <Counter
-        text="average"
-        count={
-          (good + neutral + bad) === 0
-            ? "-"
-            : (good + neutral * 0 + bad * -1) / (good + neutral + bad)}
-      />
-      <Counter
-        text="postive"
-        count={
-          (good + neutral + bad) === 0
-            ? "-"
-            : (good / (good + neutral + bad) * 100) + " %"}
-      />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </>
     )
 }
-
-const Button = ({ text, handler}) => {
-  return <button onClick={handler}>{text}</button>
-}
-
-const Counter = ({ text, count }) => <div>{text} {count}</div>
 
 export default App;
