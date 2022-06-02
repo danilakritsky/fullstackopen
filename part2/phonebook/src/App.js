0be7c94a5deebb0bdb3f817fa9d93cde937e253a
Danila Kritsky <danilakritsky@gmail.com>
2022-06-02 14:29:07 +0000

    Complete 2.11: The Phonebook Step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,7 @@
-import { useState } from 'react'
+import React from "react"
+
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
@@ -6,12 +9,18 @@ import Phonebook from './components/Phonebook'
 
 const App = () => {
   
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
+
+  useEffect(
+    () => {
+      axios
+        .get('https://danilakritsky-fullstackopen-66rx7x9xhwg6-3001.githubpreview.dev/persons')
+        .then(response => {
+          setPersons(response.data)
+        })
+      },
+    [])
+
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
--- part2/phonebook/src/components/Filter.js
+++ part2/phonebook/src/components/Filter.js
@@ -1,3 +1,5 @@
+import React from "react"
+
 const Filter = ({ newSearchTerm, newSearchTermHandler }) => {
     return (
       <div>
