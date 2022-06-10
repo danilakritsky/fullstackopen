feca08105f7cb1ac13e2aafbe67ec13a3efcf0d2
Danila Kritsky <danilakritsky@gmail.com>
2022-06-10 07:53:41 +0000

    Complete 2.14*: Data for countries, step3

--- part2/countries/src/components/CountryInfo.js
+++ part2/countries/src/components/CountryInfo.js
@@ -1,8 +1,29 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 
 const CountryInfo = ({ country }) => {
+  
+  const [ weatherData, setWeatherData ] = useState([])
+  const [ weatherDataloaded, setWeatherDataLoaded ] = useState(false)
+
+  useEffect(
+    () => {
+      axios.get(
+        `https://api.openweathermap.org/data/2.5` +
+        `/weather?q=${country.capital[0]}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
+      )
+      .then(response => {
+        setWeatherData(response.data);
+        setWeatherDataLoaded(true);
+      })
+    },
+    []
+  )
 
   return (
+    weatherDataloaded &&
     <>
       <h1>{country.name.common}</h1>
       <div>capital: {country.capital[0]}</div>
@@ -24,6 +45,17 @@ const CountryInfo = ({ country }) => {
           )
         }  
       </ul>
+      <h2>Weather in {country.capital[0]}</h2>
+      <div>temperature: {weatherData.main.temp} Celsius</div>
+      <img
+        alt={
+          `Current weather icon for ${country.capital[0]}` + 
+          `- ${weatherData.weather[0].description}`
+        }
+        src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+      >
+      </img>
+      <div>wind: {weatherData.wind.speed} m/s</div>
     </>
   )
 }
