4cc22f89c9cb3d865383d21c2ff627a333d8dc23
Danila Kritsky <danilakritsky@gmail.com>
2022-07-15 10:58:55 +0000

    Complete 4.5*: helper functions and unit tests, step3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -8,7 +8,22 @@ const totalLikes = blogs => (
       .reduce((sum, item) => sum + item, 0)
 );
 
+const favoriteBlog = blogs => {
+  if (blogs.length === 0) { return {}; }
+
+  const sortedBlogs = [...blogs].sort(
+    (blog, nextBlog) => {
+      if (blog.likes > nextBlog.likes) { return 1; }
+      if (blog.likes < nextBlog.likes) { return -1; }
+      return 0;
+    }
+  );
+  const { title, author, likes } = sortedBlogs[sortedBlogs.length - 1];
+  return { title, author, likes };
+};
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 };
