2e366e5e1d5a451d7c6bc0a1d91085453a84412b
Danila Kritsky <danilakritsky@gmail.com>
2022-09-05 12:20:07 +0000

    Complete 4.18: bloglist expansion, step6

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const blogRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
 
@@ -19,5 +20,6 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/blogs', blogRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 module.exports = app;
